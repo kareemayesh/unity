6d80352c0de46208a96075262c9a7503956993c7
kareemayesh <kska213@gmail.com>
2016-06-23 13:58:40 +0300

    create project

---
+++ database/migrations/2016_06_23_103744_create-test-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('string');
+            $table->integer('integer');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('test');
+    }
+}
